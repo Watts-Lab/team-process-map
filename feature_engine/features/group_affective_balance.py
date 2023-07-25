b30a871e3f66001974494fa8f640c592569b234f
Shruti Agarwal <agshruti@Shrutis-MBP.fios-router.home>
2023-07-25 13:09:53 -0400

    init hostile affect feature

---
+++ feature_engine/features/group_affective_balance.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+"""
+Conversation Level Feature
+
+Feature that returns the difference of overall positivity - negativity at the conversation level to assess how potential conflict affects the group discussion, as well as the general balance between positivity and negativity.
+
+"""
+
+
+def group_affective_behavior(chat_df):
+
+    gab = []
+    for conv in chat_df.groupby(['conversation_num']):
+        # print(conv[1]['positive_bert'])
+
+        gab.append(conv[1]['positive_bert'].sum() - conv[1]['negative_bert'].sum())
+    
+    return pd.DataFrame({'gab':gab})
