66b2d6824051ea790ec99a7d04ab81d359394800
Priya DCosta <76833529+PriyaDCosta@users.noreply.github.com>
2024-02-12 14:55:50 -0500

    Update politeness_v2.py

--- feature_engine/features/politeness_v2.py
+++ feature_engine/features/politeness_v2.py
@@ -19,7 +19,8 @@ def get_politeness_v2(df,on_column):
         The dataframe after adding the politness v2 features
     """
 
-    #extract the column headers by running the script for a random text. We sort the names of the features in alphabetical order
+    #extract the column headers by running the script for a random text. We sort the names of the features in alphabetical order. 
+    #This is done because the original package sorts the features by counts. It is not possible do so if we have a number of rows, as each row may have different counts for different features 
     column_headers = fe.feat_counts("hahaha",kw).sort_values(by='Features')['Features'].tolist()
     
     # Apply the function to each row in 'text_column' and store the result in a new column 'output_column'. We sort the names of the features in alphabetical order
